45d364ea92fc3c2812bcaf8fb314870526edadfe
CyberNavX <gerbergarciasalas52@gmail.com>
2026-03-08 18:04:57 -0600

    Realice CalculadoraPresupuesto en /Ejercicios

---
+++ Ejercicios/CalculadoraPresupuesto.java
@@ -0,0 +1,27 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class CalculadoraPresupuesto {
+    public static void main(String[] args) {
+        
+        // 1. Creamos el objeto que leera lo que vamos a escribir
+        Scanner leer = new Scanner(System.in);
+
+        // 2. Se piden los datos 
+        System.out.println("Ingrese el presupuesto mensual total: ");
+        double presupuesto = leer.nextDouble(); // Aqui guardamos el valor que se ingresa en Decimal
+
+        System.out.println("Ingresa los gastos fijos: ");
+        double gastos = leer.nextDouble();
+
+        // 3. La operacion para que calcule el programa lo que se pide
+        double disponible = presupuesto - gastos;
+        double porDia = disponible/30;
+
+        // 4. Imprimimos los resultados
+        System.out.println("Te quedan en Q " + disponible + " libres al mes.");
+        System.out.println("Puedes gastar maximo de Q " + porDia + " por dia.");
+    }   
+
+}
---
+++ Ejercicios/README.md
@@ -0,0 +1,7 @@
+# Ejercicio en Java - Mis inicios
+
+En esta carpeta documento mis practicas diarias para dominar la logica de programacion en este lenguaje.
+
+## Aprendizaje (8 de marzo, 2026)
+* Aprendi a usar Scanner (Comentando la explicacion dentro de CalculadoraPresupuesto)
+* Entendi a usar paquetes en java, 'package Ejercicios;', la ejecucion se debe hacer desde la carpeta raiz>>,del proyecto, en este caso es Java (repositorio de GitHub). 
